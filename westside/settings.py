82b3a011072956d32bca50cea62ed830c2ae3c57
jonas-yaw <knkmjonas@gmail.com>
2022-08-29 09:58:49 -0700

    media setting added

--- westside/settings.py
+++ westside/settings.py
@@ -134,6 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'home'
 
