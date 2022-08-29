b3c1996002365ed3509a44e26bb2d9051fae353b
jonas-yaw <knkmjonas@gmail.com>
2022-08-29 09:53:07 -0700

    runtime url updated

--- westside/urls.py
+++ westside/urls.py
@@ -17,6 +17,10 @@ from re import template
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic.base import TemplateView
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +29,8 @@ urlpatterns = [
     path('users/',include('django.contrib.auth.urls')),
     path('',include('students.urls'))
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
