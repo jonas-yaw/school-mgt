fc7f9bec4493958ffabdd7a0c320a0d4bcc337f4
jonas-yaw <knkmjonas@gmail.com>
2022-08-31 17:18:38 -0700

    origins changed

--- westside/settings.py
+++ westside/settings.py
@@ -145,6 +145,5 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://obscure-savannah-12473.herokuapp.com',
     'https://sure-start-is.herokuapp.com/'
 ]
